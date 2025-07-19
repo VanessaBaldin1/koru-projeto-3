20255a8cd573e120d4c5b9845a3834d2d8ccf86b
Vanessa Baldin <vanbaldin08@gmail.com>
2025-07-19 11:47:57 -0300

    Criando as pastas organização
    Edita Readme, cria pastas e incluindo as resoluções

---
+++ exercicio-1/hello.js
@@ -0,0 +1,22 @@
+//Resolução do Exercicio-1
+
+//Crie o arquivo hello.js
+
+// Acesse o terminal ou terminal do VSCODE
+// Acesse a pasta onde esta o arquivo usando o comando no terminal 
+   // cd caminho/da/pasta
+
+// Execute o arquivo usando node
+   // node hello.js
+
+// Tem que aparece as seguintes informações no terminal
+
+// Informações que constam no meu notebook
+
+// Hello, Node.js!
+// Versão do Node.js: v22.14.0
+// Diretório atual: C:\koru-projeto-3\exercicio-1
+
+console.log("Hello, Node.js!"); // Exibe uma mensagem no terminal
+console.log("Versão do Node.js:", process.version); // Mostra a versão instalada do Node
+console.log("Diretório atual:", process.cwd()); // Mostra o caminho da pasta onde o script está sendo executado
